ed48b255eb48624693decd4b887222b85e2ec217
MohaimanulIslam <mohaimanul.cse@gmail.com>
2022-02-04 11:43:20 +0600

    Correction on first TC

--- Tests/LoginTest.py
+++ Tests/LoginTest.py
@@ -6,6 +6,6 @@ class LoginTest(BasePage):
     def test_login_page(self):
         loginPage = LoginPage(self.driver)
         loginPage.click_on_login()
-        # loginPage.enter_email_id("marazislam@gmail.com")
+        loginPage.enter_email_id("marazislam@gmail.com")
 
 
